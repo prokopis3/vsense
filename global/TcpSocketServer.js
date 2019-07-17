93afaac962fe72ca8d4d4c541d4496b43383bb6b
prokopis3 <prokopis123@gmail.com>
2019-07-17 13:20:15 +0300

    start first set tcp server tk103A on port 8081

--- global/TcpSocketServer.js
+++ global/TcpSocketServer.js
@@ -1,14 +1,48 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var _1 = require(".");
+var gps = require('gps-tracking');
 var TcpSocketServer = /** @class */ (function () {
     function TcpSocketServer(svraddr, svrport) {
         var gbr = new _1.GBRoutines();
         this.svraddr = gbr.Variablevalid(svraddr) == null ? '0.0.0.0' : svraddr;
         this.svrport = gbr.Variablevalid(svrport) == null ? 8000 : svrport;
         this.sockets = [];
+        this.gpsOptions = {
+            'debug': false,
+            'port': 8081,
+            'device_adapter': "TK103"
+        };
     }
-    TcpSocketServer.prototype.TcpServer = function () {
+    TcpSocketServer.prototype.TcpGpsTrack = function () {
+        var server = gps.server(this.gpsOptions, function (device, connection) {
+            device.on("connected", function (data) {
+                console.log("I'm a new device connected" + (" " + data));
+                return data;
+            });
+            device.on("login_request", function (device_id, msg_parts) {
+                console.log('Hey! I want to start transmiting my position. Please accept me. My name is ' + device_id);
+                device.login_authorized(true);
+                console.log("Ok, " + device_id + ", you're accepted!");
+            });
+            device.on("ping", function (data) {
+                //this = device
+                console.log("I'm here: " + data.latitude + ", " + data.longitude + " (" + device.getUID() + ")");
+                //Look what informations the device sends to you (maybe velocity, gas level, etc)
+                //console.log(data);
+                return data;
+            });
+            device.on("alarm", function (alarm_code, alarm_data, msg_data) {
+                console.log("Help! Something happend: " + alarm_code + " (" + alarm_data.msg + ")");
+            });
+            //Also, you can listen on the native connection object
+            connection.on('data', function (data) {
+                //echo raw data package
+                console.log(data.toString());
+            });
+        });
+    };
+    TcpSocketServer.prototype.TcpServertk102 = function () {
         this.server = require('tk102');
         // start server 
         this.server.createServer({
@@ -91,4 +125,6 @@ var TcpSocketServer = /** @class */ (function () {
     };
     return TcpSocketServer;
 }());
-exports.default = new TcpSocketServer();
+var tcpsock = new TcpSocketServer();
+tcpsock.TcpGpsTrack();
+exports.default = tcpsock;
